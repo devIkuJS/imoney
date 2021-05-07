a87ad5da2e5803608aabcb9790dbeccae5bee98c
devIkuJS <brian125865@gmail.com>
2021-05-07 14:39:50 -0500

    feat: update crud cuenta bancaria controller blade

--- resources/views/cuentaBancaria.blade.php
+++ resources/views/cuentaBancaria.blade.php
@@ -56,11 +56,8 @@
 </style>
 @extends('layouts.app')
 
-@section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
-    <!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.bootstrap4.min.css">-->
-@endsection
+@section('content_header')
+
 
 @section('content')
 <main>
@@ -69,15 +66,21 @@
         <div class="row">        
             <div class="col-md-12 mx-auto">
                 <div class="card">
-                    <h3 class="card-title">Listado de Cuentas Bancarias</h3>    
+                
+                    <h3 class="card-title text-white font-weight-bold">Listado de Cuentas Bancarias</h3> 
+                    <h1>
+                    <!--Cuentas-->
+                    <button type="button" class="btn btn-dark ml-2" data-toggle="modal"
+                            data-target="#modal-crear-cuenta"><i class="fa fa-plus-circle pr-2"></i>Crear cuenta
+                     </button>
+                    </h1>   
 
                 <div class="card-body">
                   <table id="cuentaBancaria" class="table table-striped table-bordered shadow-lg mt-4 dt-responsive nowrap" style="width:100%">
-                    <thead class="bg-success text-white">
+                    <thead class="bg-secondary text-white">
                         <tr>
                         
                             <th>ID</th>
-                            <!--<th>Usuario</th>-->
                             <th>Banco</th>
                             <th>Tipo de Cuenta</th>
                             <th>Categoria de Cuenta</th>
@@ -86,21 +89,26 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($lista_cuentas as $lista_cuenta)
+                        @foreach ($lista_cuentas as $cuentaBancaria)
                         <tr>
-
-                            <td>{{ $lista_cuenta->id }}</td> 
-                            <td>{{ $lista_cuenta->banco }}</td>
-                            <td>{{ $lista_cuenta->tipo }}</td>
-                            <td>{{ $lista_cuenta->categoria }}</td>
-                            <td>{{ $lista_cuenta->numero_cuenta }}</td>       
+                            <td>{{ $cuentaBancaria->id }}</td> 
+                            <td>{{ $cuentaBancaria->banco }}</td>
+                            <td>{{ $cuentaBancaria->tipo }}</td>
+                            <td>{{ $cuentaBancaria->categoria }}</td>
+                            <td>{{ $cuentaBancaria->numero_cuenta }}</td>       
                             <td>
           
+                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modal-update-cuentaBancaria-{{$cuentaBancaria->id}}">Editar</button>
+                                <form action="{{ route ('cuentaBancaria.eliminar', $cuentaBancaria->id) }}" class="d-inline" method="post">
+                                    {{ csrf_field() }}
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Eliminar</button>
+                                    
                                 </form>
                             </td>
                         </tr>
                         <!-- modal update -->
-                       
+                        @include('modal-update-cuentaBancaria')
                         <!-- /.modal update-->
                         @endforeach
                     </tbody>
@@ -121,7 +129,60 @@
     <!-- /.row -->   
 </main>
 
-
+<!-- Modal Agregar Cuenta -->
+<div class="modal fade" id="modal-crear-cuenta" tabindex="-1" role="dialog" aria-labelledby="modal-crear-cuenta"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-crear-cuenta">Crear nueva cuenta
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('cuentaBancaria.registro') }}" method="POST">
+                {{ csrf_field() }}
+            <p class="text-center font-weight-bold mt-3" style="color:#2a3253;font-size: 17px;"></p>
+            <div class="modal-body">
+                
+                <div class="form-group">
+                    <select class="form-control" id="cuenta_bancaria_user" name="cuenta_bancaria_user">
+                        <option value="">Seleccione el banco</option>
+                        @foreach ($bancos as $banco)
+                        <option value="{{$banco->id}}">{{$banco->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" id="tipo_cuenta" name="tipo_cuenta">
+                        <option value="">Seleccione el tipo de cuenta</option>
+                        @foreach ($tipo_cuenta as $tipo)
+                        <option value="{{$tipo->id}}">{{$tipo->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="numero_cuenta">Numero de cuenta</label>
+                    <input type="text" class="form-control" id="numero_cuenta" name="numero_cuenta">
+                </div>
+                <div class="form-group">
+                    <select class="form-control" id="categoria_cuenta" name="categoria_cuenta">
+                        <option value="">Seleccione la categoria de la cuenta</option>
+                        @foreach ($categoria_cuenta as $cat)
+                        <option value="{{$cat->id}}">{{$cat->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div id="success-message"></div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" id="crear-tipo-cuenta">Agregar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Agregar Cuenta -->
 
 
     @endsection
