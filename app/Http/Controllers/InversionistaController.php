f32975cad32b207ff855e28ec4d47851788b19e1
Brian <brian125865@gmail.com>
2021-05-25 15:15:10 -0500

    feature: get datos empresas inversiones

--- app/Http/Controllers/InversionistaController.php
+++ app/Http/Controllers/InversionistaController.php
@@ -6,10 +6,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Inversionista;
 use App\Models\EmpresaInversiones;
+use DateTime;
 
 
 class InversionistaController extends Controller
 {
+   
+
     public function __construct()
     {
         $this->middleware('auth');
@@ -17,9 +20,30 @@ class InversionistaController extends Controller
 
     public function index()
     {
-        $inversiones = EmpresaInversiones::all();
+        $empresas = EmpresaInversiones::all();
+
+       // $newEmpresas = []; 
+        //dd($this->dateDiff($empresas[1]["fecha_esperada"], now()));
+
+       /* foreach($empresas as $key=>$value) {
+            $obj->fecha_esperada = $this->dateDiff($empresas["fecha_esperada"], now());
+        }
+        */
+
+        for ($i = 0; $i < count($empresas); $i++) {
+            $empresas[$i]["fecha_esperada"] = $this->dateDiff($empresas[$i]["fecha_esperada"], now());
+           //echo $empresas[$i]["fecha_esperada"]."\n";
+           //$empresas->fecha_esperada = $this->dateDiff($empresas["fecha_esperada"], now());
+        }
+        //dd($empresas[0]);
+
         $inversionista = Inversionista::all();
-        return view('inversionista');     
+        return view('inversionista', ['empresas' => $empresas]);     
+    }
+
+    function dateDiff ($d1, $d2) {
+        // Return the number of days between the two dates:    
+        return round(abs(strtotime($d1) - strtotime($d2))/86400);
     }
     
     protected function downloadFile($src){
--- resources/views/inversionista.blade.php
+++ resources/views/inversionista.blade.php
@@ -13,114 +13,125 @@
         border-radius: 10px;
 
     }
-   
+
     /* Activado */
-    .custom-control-input:checked~.custom-control-label::before{
+    .custom-control-input:checked~.custom-control-label::before {
         border-color: black !important;
         background-color: black !important;
-    
+
     }
+
     /* Desactivado*/
-    .custom-control-label:before{
-        border-color:green !important;
-        background-color:green !important;
+    .custom-control-label:before {
+        border-color: green !important;
+        background-color: green !important;
     }
-    .btn-danger{
+
+    .btn-danger {
         /*background: transparent !important;*/
     }
 
     @media (max-width: 575.98px) {
         h4 {
-            font-size:15px !important;
+            font-size: 15px !important;
         }
-        
-        div.card{
+
+        div.card {
             width: 4rem !important;
         }
-        h5{
-            font-size:14px !important;  
+
+        h5 {
+            font-size: 14px !important;
         }
     }
 
     @media (max-width: 767.98px) {
-        h4{
-            font-size:15px !important; 
+        h4 {
+            font-size: 15px !important;
         }
-        h5{
-            font-size:15px !important; 
+
+        h5 {
+            font-size: 15px !important;
         }
-        span.h3{
-            font-size:22px !important; 
+
+        span.h3 {
+            font-size: 22px !important;
         }
+
         div.card {
-            font-size:10px !important;
+            font-size: 10px !important;
         }
     }
 
     @media (max-width: 991.98px) {
-        h4{
-            font-size:17px !important; 
+        h4 {
+            font-size: 17px !important;
         }
+
         div.card {
-            width:10rem !important;
+            width: 10rem !important;
         }
     }
 
     @media (max-width: 1199.98px) {
-        h4{
-            font-size:17px !important; 
+        h4 {
+            font-size: 17px !important;
         }
+
         div.card {
-            width:10rem !important;
+            width: 10rem !important;
         }
     }
 
 
-    .form-control{
-        background-color:transparent !important;
-        border:none !important       
+    .form-control {
+        background-color: transparent !important;
+        border: none !important
     }
 
-    .input-monto{
+    .input-monto {
         outline: 0 !important;
         /*color: #fff !important;*/
         border-bottom: 3px solid white !important;
-        font-size:24px !important;
+        font-size: 24px !important;
     }
-    input:focus, input.form-control:focus {
-        outline:none !important;
+
+    input:focus,
+    input.form-control:focus {
+        outline: none !important;
         outline-width: 0 !important;
         box-shadow: none;
         -moz-box-shadow: none;
         -webkit-box-shadow: none;
     }
-    #amount-monto {  
-        color:white;
+
+    #amount-monto {
+        color: white;
     }
 
     #amount-monto::placeholder {
-    color:white;
+        color: white;
     }
 
     #amount-monto::-webkit-input-placeholder {
-    color: white;
+        color: white;
     }
 
     #amount-monto::-moz-placeholder {
-    color: white;
+        color: white;
     }
 
     #amount-monto:-ms-input-placeholder {
-    color: white;
+        color: white;
     }
-    
+
     #amount-monto:-moz-placeholder {
-    color: white;
+        color: white;
     }
-    .col-form-label{
-        background: white !important;       
+
+    .col-form-label {
+        background: white !important;
     }
-   
 </style>
 
 
@@ -135,21 +146,21 @@
             <div class="col-md-12 text-center">
                 <span class="font-weight-bold h2">Hola, </span><span
                     class="font-weight-bold h2">{{ Auth::user()->name }}</span>
-                    <span class="font-weight-bold text-black h2">este es tu saldo disponible:</span>        
+                <span class="font-weight-bold text-black h2">este es tu saldo disponible:</span>
             </div>
             <div class="col-md-12 mx-auto">
-                    <div class="row">
-                        <div class="col-6 text-right">
-                            <h4 class="font-weight-bold">PEN 0.00</h4> 
-                        </div>
-                        <div class="col-6 text-left">
-                            <h4 class="font-weight-bold">USD 0.00</h4>
-                        </div>
+                <div class="row">
+                    <div class="col-6 text-right">
+                        <h4 class="font-weight-bold">PEN 0.00</h4>
                     </div>
-            <div>
+                    <div class="col-6 text-left">
+                        <h4 class="font-weight-bold">USD 0.00</h4>
+                    </div>
+                </div>
+                <div>
 
-                <div class="col-md-12 mx-auto">
-                    <div class="row mt-5">
+                    <div class="col-md-12 mx-auto mb-5">
+                        <div class="row mt-5">
                             <div class="col-6 text-left">
                                 <h4 class="text-white font-weight-bold">Oportunidades de Inversión</h4>
                             </div>
@@ -158,140 +169,67 @@
                                     <span class="mr-2 font-weight-bold text-white">PEN</span>
                                     <div class="custom-control custom-switch">
                                         <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                                        <label class="custom-control-label text-white font-weight-bold" for="customSwitch1">USD</label>  
-                                    </div>   
-                                </div>
-                            </div>  
-                    </div>
-
-                    <div class="row mt-5">
-                            <div class="col-6 text-left">
-                                <h4 class="text-white font-weight-bold">Empresa pagadora</h4>
-                                    <img src={{asset('imagenes_empresa/imoney.jpg')}} width="120"
-                                        height="70" class="mr-3">
-                            </div>
-                            <div class="col-6 text-right">
-                                <h4 class="text-white font-weight-bold">Bertonati Technologies S.A.</h4>
-                                    <img src={{asset('imagenes_empresa/rojo.jpg')}} width="120"
-                                     height="70" class="">
-                            </div>
-                    </div>
-
-                    <div class="row mt-5">
-                            <div class="col-6 text-left">
-                                <h5 class="text-white font-weight-bold">Monto disponible</h5>
-                                <h5 class="text-white font-weight-bold">106,721.92</h5>
-                            </div>
-                            <div class="col-6 text-right">
-                                <h5 class="text-white font-weight-bold">Monto total</h5>
-                                <h5 class="text-white font-weight-bold">144,432.00</h5>
-                            </div>
-                    </div>
-
-                    <div class="row text-center">
-                            <div class="col-12">
-                                <div class="progress">
-                                    <div id="inversionista" class="progress-bar bg-dark" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
+                                        <label class="custom-control-label text-white font-weight-bold"
+                                            for="customSwitch1">USD</label>
+                                    </div>
                                 </div>
                             </div>
-                    </div>
+                        </div>
 
-                    <!--<div class="row">
-                            <div class="col-6">
-                                <div class="d-flex justify-content-center align-items-center border-content-change">
-                                    <label class="form-control-label font-weight-bold" for="envias">106,721.92</label>
+                        @foreach ($empresas as $empresa)
+                        <div>
+                            <div class="row mt-5">
+                                <div class="col-6 text-left">
+                                    <h4 class="text-white font-weight-bold">Empresa pagadora</h4>
+                                    <img src={{asset('imagenes_empresa/imoney.jpg')}} width="120" height="70" class="mr-3">
+                                </div>
+                                <div class="col-6 text-right">
+                                    <h4 class="text-white font-weight-bold">{{ $empresa->nombre }}</h4>
+                                    <img src={{asset($empresa->logo)}} width="120" height="70" >
                                 </div>
                             </div>
-
-                            <div class="col-6">
-                                <div class="d-flex justify-content-center align-items-center border-content-change">
-                                    <label class="form-control-label font-weight-bold" for="envias">144,432.00</label>
+    
+                            <div class="row mt-5">
+                                <div class="col-6 text-left">
+                                    <h5 class="text-white font-weight-bold">Monto disponible</h5>
+                                    <h5 class="text-white font-weight-bold">{{ $empresa->monto_disponible }}</h5>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <h5 class="text-white font-weight-bold">Monto total</h5>
+                                    <h5 class="text-white font-weight-bold">144,432.00</h5>
                                 </div>
                             </div>
-                    </div>-->
-       
-
-                    <div class="row mt-4">
-                        <div class="col-6">
-                            <h5 class="text-white text-left font-weight-bold">Tasa anualizada</h5>
-                            <div class="card font-weight-bold py-2 w-50 text-center float-left">13.63%</div>
-                        </div>
-                        <div class="col-6">
-                            <h5 class="text-white text-right font-weight-bold ">Plazo</h5>
-                            <div class="card font-weight-bold py-2 w-50 text-center float-right">113 días</div>
-                        </div> 
-                    </div>
-                    <div class="row mt-5">
-                    <div class="col-md-12 text-left mt-4">
-                        <span class="font-weight-bold h2">Característica de la Inversión - Deuda </span>            
-                    </div>
-                    </div>
-
-                        <div class="row mt-4">
-                            <div class="col-6">
-                                    <!--<h5 class="text-white text-left font-weight-bold">Tasa anualizada</h5>
-                                    <div class="card font-weight-bold py-2 w-50 text-center float-left">13.63%</div>-->
-                                    <h5 class="text-white text-left font-weight-bold">Fecha proyectada de cobro</h5>
-                                    <div class="card font-weight-bold py-2 w-50 text-center float-left">{{ date('d-m-Y', strtotime(now())) }}</div>
+    
+                            <div class="row text-center">
+                                <div class="col-12">
+                                    <div class="progress">
+                                        <div id="inversionista" class="progress-bar bg-dark" role="progressbar"
+                                            style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-6">
-                                    <h5 class="text-white text-right font-weight-bold">Descarga el</h5>
-                                    <!--<h5 class="text-white text-right font-weight-bold">Fecha proyectada de cobro</h5>
-                                    <div class="card font-weight-bold py-2 w-50 text-center float-right">{{ date('d-m-Y', strtotime(now())) }}</div>-->
-                                    <a href="{{'inversionista/download'}}"><button type="button" class="btn btn-danger float-right"><i class="far fa-file-pdf"></i>&nbsp;Teaser Informativo</button></a>
-                                    <!--<onclick="window.open('file.pdf')">-->
-                            </div> 
-                        </div>
-
-                    <div class="row mt-4">
-                        <div class="col-6">
-                                <h5 class="text-white text-left font-weight-bold">Tasa mensual</h5>
-                                <div class="card font-weight-bold py-2 w-50 text-center float-left">1.0706%</div>
-                        </div>
-                        <div class="col-6">
-                                <h5 class="text-white text-right font-weight-bold">Numero de factura</h5>
-                                <div class="card font-weight-bold py-2 w-50 text-center float-right">F0001-00000076</div>
-                        </div> 
-                    </div>
-
-            
-                    <div class="row mt-5">
-                        <div class="col-md-12 mt-4 text-left">
-                            <span class="font-weight-bold h3">Operación de Factoring</span>            
-                        </div>
-                    </div>
-
-                
-                    <div class="row mt-4">
-                            <div class="col-12 text-left">
-                                <h4 class="text-white font-weight-bold ">Quiero Invertir</h4>
-                                <input type="text" id="amount-monto" placeholder="Monto" maxlength="4" class="form-control input-monto font-weight-bold"
-                                    onkeypress="return isNumber(event);"/>
-                                    <h5 class="text-black text-center font-weight-bold">(cuentas con USD 0.00 disponibles)</h5>
-                            </div>  
-                    </div>
-
-                    <div class="row mt-4">
-                            <div class="col-6">
-                                <h5 class="text-white font-weight-bold text-left">Monto disponible de la factura</h5>
-                                <h5 class="text-white font-weight-bold text-left">106,721.92</h5>
+    
+                            <div class="row mt-4">
+                                <div class="col-3">
+                                    <h5 class="text-white text-left font-weight-bold">Tasa anualizada</h5>
+                                    <div class="card font-weight-bold py-2 w-50 text-center float-left">8%</div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="text-center mt-4 ">
+                                        <button class="btn btn-primary btn-cambiar-ahora">Invertir</button>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <h5 class="text-white text-right font-weight-bold ">Plazo</h5>
+                                    <div class="card font-weight-bold py-2 w-50 text-center float-right">{{ $empresa->fecha_esperada }} días</div> 
+                                </div>
                             </div>
-                            <div class="col-6">
-                                <h5 class="text-white font-weight-bold text-right">Retorno esperado</h5>
-                                <h5 class="text-white font-weight-bold text-right">+USD 0.00</h5>
-                                <div class="card font-weight-bold py-2 w-50 text-center float-right">113 dias</div>
-                                <!--<input type="text" class="col-form-label text-right rounded-pill float-right" placeholder="113 dias" disabled></input>-->
-                            </div>
-                    </div>
-
-                    <div class="row mt-4">
-                        <div class="text-center mt-4">
-                            <button class="btn btn-primary btn-cambiar-ahora" type="button" id="cambiarAhora" onclick="move()">Invertir
-                            </button>
                         </div>
+                        @endforeach
                     </div>
-
-
+                </div>
+            </div>
         </div>
     </div>
 </main>
@@ -300,7 +238,7 @@
 
 @section('custom-script')
 <script>
-var i = 0;
+    var i = 0;
 function move() {
   if (i == 0) {
     i = 1;
@@ -328,4 +266,3 @@ function isNumber(evt) {
 }
 </script>
 @stop()
-
